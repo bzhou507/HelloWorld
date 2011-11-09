a44940c777de4b58b4fda038c65dc6f5bd7e8c59
Bobby Zhou <bobbyzhou@attask.com>
2011-11-09 14:12:13 -0700

    Prints hello world to console

---
+++ src/main.java
@@ -0,0 +1,12 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: Bobby Zhou
+ * Date: 11/9/11
+ * Time: 2:00 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class main {
+    static void main(String[] args){
+        System.out.println("HELLO WORLD");
+    }
+}
